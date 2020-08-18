7ac49711b99fd78bcec4832559d3d4938b9e21e4
Sunny <2367824384@qq.com>
2020-08-18 21:01:25 +0800

    submit

---
+++ StockMarketFrontEnd/src/app/Mock/companylist.ts
@@ -0,0 +1,67 @@
+import { COMPANY } from '../Model/company'
+export const CompanyList: COMPANY[] = [{
+    id: 1001,
+    company_code: '32001',
+    company_name: 'MCD',
+    turnover: 100000.00,
+    ceo: 'Tony',
+    board_of_director: 'user',
+    listed_in_se: 'No',
+    sector_name: 'seller',
+    brife_write_up: 'Food',
+    stock_code: '6001',
+    company_status: 'Actived'
+},
+{
+    id: 1002,
+    company_code: '32001',
+    company_name: 'KFC',
+    turnover: 200000.00,
+    ceo: 'Jack',
+    board_of_director: 'Group director',
+    listed_in_se: 'Yes',
+    sector_name: 'buyer',
+    brife_write_up: 'Food',
+    stock_code: '6002',
+    company_status: 'Inactive'
+},
+{
+    id: 1003,
+    company_code: '32003',
+    company_name: 'ADDIDAS',
+    turnover: 100000.00,
+    ceo: 'Tom',
+    board_of_director: 'Group director',
+    listed_in_se: 'Yes',
+    sector_name: 'seller',
+    brife_write_up: 'Food',
+    stock_code: '6001',
+    company_status: 'Actived'
+},
+{
+    id: 1004,
+    company_code: '32001',
+    company_name: 'ADDIDAS',
+    turnover: 100000.00,
+    ceo: 'Tony',
+    board_of_director: 'Group director',
+    listed_in_se: 'Yes',
+    sector_name: 'seller',
+    brife_write_up: 'Food',
+    stock_code: '6001',
+    company_status: 'Actived'
+},
+{
+    id: 1005,
+    company_code: '32001',
+    company_name: 'ADDIDAS',
+    turnover: 100000.00,
+    ceo: 'yancy',
+    board_of_director: 'Group director',
+    listed_in_se: 'Yes',
+    sector_name: 'seller',
+    brife_write_up: 'Food',
+    stock_code: '6001',
+    company_status: 'Actived'
+}
+]
---
+++ StockMarketFrontEnd/src/app/Mock/ipoList.ts
@@ -0,0 +1,62 @@
+import { IPO } from '../Model/ipo'
+
+
+export const IPOlist: IPO[] = [
+    {
+        id: 1,
+        company_name: 'KFC',
+        stock_exchange: 'ASE',
+        price_per_share: 356.23,
+        total_num: '1356.23',
+        open_date_time: new Date(),
+        remark: 'REMARK1'
+    },
+    {
+        id: 2,
+        company_name: 'KFC',
+        stock_exchange: 'ASE',
+        price_per_share: 356.23,
+        total_num: '1356.23',
+        open_date_time: new Date(),
+        remark: 'REMARK1'
+    },
+    {
+        id: 3,
+        company_name: 'KFC',
+        stock_exchange: 'ASE',
+        price_per_share: 356.23,
+        total_num: '1356.23',
+        open_date_time: new Date(),
+        remark: 'REMARK1'
+    },
+    {
+        id: 4,
+        company_name: 'KFC',
+        stock_exchange: 'ASE',
+        price_per_share: 356.23,
+        total_num: '1356.23',
+        open_date_time: new Date(),
+        remark: 'REMARK1'
+    }
+    ,
+    {
+        id: 5,
+        company_name: 'KFC',
+        stock_exchange: 'ASE',
+        price_per_share: 356.23,
+        total_num: '1356.23',
+        open_date_time: new Date(),
+        remark: 'REMARK1'
+    }
+    ,
+    {
+        id: 6,
+        company_name: 'KFC',
+        stock_exchange: 'ASE',
+        price_per_share: 356.23,
+        total_num: '1356.23',
+        open_date_time: new Date(),
+        remark: 'REMARK1'
+    }
+
+];
